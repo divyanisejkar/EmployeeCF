d3116a4b930775882a5a17cc71cc4831ef6358d2
Divyani.Sejkar <sejkar24@gmail.com>
2019-11-29 11:35:58 +0530

    Add project files.

---
+++ Migrations/20191128131335_addsalary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeCF.Migrations
+{
+    public partial class addsalary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "salary",
+                table: "Employees",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "salary",
+                table: "Employees");
+        }
+    }
+}
